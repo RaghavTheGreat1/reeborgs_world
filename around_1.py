66abc70f59dfc6caa4a8b1d769382221e1231542
Raghav Joshi <28825619+RaghavTheGreat1@users.noreply.github.com>
2022-05-01 03:23:56 +0530

    Solution to around 1

---
+++ around_1.py
@@ -0,0 +1,8 @@
+def move_single_track():
+    for i in range(0, 10, 1):
+            if(i == 9):
+                turn_left()
+            else: 
+                move()
+for i in range(0, 4, 1):
+    move_single_track()
