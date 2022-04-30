65f4e6eb8c209a7ece029dc39271de50cd8d09f3
Raghav Joshi <28825619+RaghavTheGreat1@users.noreply.github.com>
2022-04-30 13:56:00 +0530

    Solution to hurdle 3

---
+++ hurdle_3.py
@@ -0,0 +1,37 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def move_and_left():
+    move()
+    turn_left()
+
+    
+def right_and_move():
+    turn_right()
+    move()
+    
+def left_and_move():
+    turn_left()
+    move()
+    
+# Observations: 
+# jump when wall is next to it
+# reposition to normal after jump
+
+def jump():
+    left_and_move()
+    right_and_move()
+    right_and_move()
+    turn_left()
+
+while at_goal() == False:
+    if(wall_in_front()):
+        jump()
+    else:
+        move()
+
+    
+    
+    
