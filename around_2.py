bdebf97015d938b158d553f6571df6dc9bd70f40
Raghav Joshi <28825619+RaghavTheGreat1@users.noreply.github.com>
2022-05-01 20:05:20 +0530

    Create around_2.py

---
+++ around_2.py
@@ -0,0 +1,22 @@
+# initial postion of bot
+initial_x = 4
+# number of rows
+x = 10
+# number of columns
+y = 10
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+for i in range(initial_x,  (2*x) + (2*y) + initial_x + 1, 1):
+    if(front_is_clear() and right_is_clear() == False):
+        move()
+    elif right_is_clear(): 
+        turn_right()
+        move()
+    else:
+        turn_left()
+        
+        
